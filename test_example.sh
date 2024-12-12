b8261a55ecb084d98013071f28542dd38a2e3118
yuan <yuan_xin_yu@hotmail.com>
2024-12-12 18:38:53 +0800

    remove unused fluid_posbp_tab

--- test_example.sh
+++ test_example.sh
@@ -1,4 +1,6 @@
 cmake -S . -B Debug -DCMAKE_BUILD_TYPE=Debug
 cmake --build Debug/
+# cmake -S . -B Release -DCMAKE_BUILD_TYPE=Release
+# cmake --build Release/
 gcc example/src/main.c -g -Iinclude -IDebug -LDebug -lfluidlite -lm -o fluidlite-test
 valgrind --tool=massif ./fluidlite-test /mnt/c/tools/fluidsynth/bin/GMGSx.sf2 output.pcm
