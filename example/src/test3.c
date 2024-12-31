42fd1f656e2c433673b63b2cf7d25c0fc0616ff2
yuan <yuan_xin_yu@hotmail.com>
2024-12-31 17:44:24 +0800

    m

--- example/src/test3.c
+++ example/src/test3.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[]) {
     }
 
     fluid_settings_t* settings = new_fluid_settings();
-    fluid_settings_setstr(settings, "synth.verbose", "yes"); //在新版本中"synth.verbose"是int型
+    fluid_settings_setstr(settings, "synth.verbose", "no"); //在新版本中"synth.verbose"是int型
     fluid_settings_setint(settings, "synth.polyphony", POLYPHONY); 
     fluid_settings_setint(settings, "synth.midi-channels", 1); 
     fluid_settings_setstr(settings, "synth.reverb.active", "no");
@@ -49,15 +49,15 @@ int main(int argc, char *argv[]) {
     int16_t *buffer = calloc(SAMPLE_SIZE, NUM_SAMPLES);
     FILE* file = argc > 2 ? fopen(argv[2], "wb") : stdout;
 
-    void play_one_second(int i){
-    	fluid_synth_noteon(synth, 0, i, 127);
+    void play_seconds(int value, int seconds){
+    	fluid_synth_noteon(synth, 0, value, 127);
 		int j=0;
-		for(; j <= MIRCO_SECOND/sample_duration_us(); j++){
+		for(; j <= seconds*MIRCO_SECOND/sample_duration_us(); j++){
 			fluid_synth_write_s16_mono(synth, NUM_FRAMES, buffer);
 			fwrite(buffer, SAMPLE_SIZE, NUM_SAMPLES, file);
 		}
-		printf("i:%d, j:%d\n", i, j);
-		fluid_synth_noteoff(synth, 0, i);
+		printf("value:%d, j:%d\n", value, j);
+		fluid_synth_noteoff(synth, 0, value);
     }
 
     for(int i=-2; i<3; i++){
@@ -67,7 +67,7 @@ int main(int argc, char *argv[]) {
             int v = notes[j];
             v = v + i * 12;
             printf("note value:%d\n", v);
-            play_one_second(v);
+            play_seconds(v, 2);
         }
     }
 
