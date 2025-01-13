631a3d22f6a0b2b4ed01f900e2e98335a3510bf7
yuan <yuan_xin_yu@hotmail.com>
2025-01-14 06:36:51 +0800

    ignore rm error

--- test_example.sh
+++ test_example.sh
@@ -6,13 +6,13 @@ cmake --build Debug/
 # cmake -S . -B Release -DCMAKE_BUILD_TYPE=Release
 # cmake --build Release/
 
-rm instruments-test
+rm instruments-test || true
 gcc example/src/instruments.c -g -Iinclude -Isrc -IDebug -LDebug -lfluidlite -lm -o instruments-test
 ./instruments-test ./example/sf_/Boomwhacker.sf2
 
-rm fluidlite-test
+rm fluidlite-test || true
 gcc example/src/main.c -g -Iinclude -IDebug -LDebug -lfluidlite -lm -o fluidlite-test
-rm massif.out.*
+rm massif.out.* || true
 valgrind --tool=massif ./fluidlite-test ./example/sf_/GMGSx_1.sf2 output.pcm
 # ms_print massif.out.<pid>
 # ffmpeg -f s16le -ar 44100 -ac 2 -i output.pcm output.wav
@@ -27,7 +27,7 @@ valgrind --tool=massif ./fluidlite-test ./example/sf_/GMGSx_1.sf2 output.pcm
 # SPL 和 dBFS 之间没有直接的数学关系，因为 SPL 取决于播放设备的增益和环境的声学特性。
 
 
-rm mono-test
+rm mono-test || true
 gcc example/src/mono.c -g -Iinclude -Isrc -IDebug -LDebug -lfluidlite -lm -o mono-test
 ./mono-test ./example/sf_/Boomwhacker.sf2 mono.pcm
 # ffmpeg -f s16le -ar 44100 -ac 1 -i mono.pcm mono.wav
@@ -37,12 +37,12 @@ gcc example/src/mono.c -g -Iinclude -Isrc -IDebug -LDebug -lfluidlite -lm -o mon
 # valgrind --tool=massif --massif-out-file=massif_test2 ./test2
 
 
-rm sfload_mem
+rm sfload_mem || true
 gcc example/src/sfload_mem.c -g -Iinclude -Isrc -IDebug -LDebug -lfluidlite -lm -o sfload_mem
 valgrind --tool=massif --massif-out-file=massif_sfload_mem ./sfload_mem mem.pcm
 # massif-visualizer massif_sfload_mem
 
-rm test3
+rm test3 || true
 gcc example/src/test3.c -g -Iinclude -Isrc -IDebug -LDebug -lfluidlite -lm -o test3
 ./test3 example/sf_/GMGSx_1.sf2 test3.pcm
 # ffmpeg -f s16le -ar 44100 -ac 1 -i test3.pcm test3.wav
