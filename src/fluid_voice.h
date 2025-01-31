cf1e8c2a69aafd2adae7b00ebaa2ff1ef85c89fb
yuan <yuan_xin_yu@hotmail.com>
2025-01-31 21:09:57 +0800

    int -> uint8_t

--- src/fluid_voice.h
+++ src/fluid_voice.h
@@ -42,41 +42,18 @@ struct _fluid_voice_t {
     unsigned int id; /* the id is incremented for every new noteon.
         it's used for noteoff's  */
     unsigned char status;
-    unsigned char
-        chan; /* the channel number, quick access for channel messages */
-    unsigned char key; /* the key, quick acces for noteoff */
-    unsigned char vel; /* the velocity */
+    unsigned char chan; /* the channel number, quick access for channel messages */
+    unsigned char key;  /* the key, quick acces for noteoff */
+    unsigned char vel;  /* the velocity */
     fluid_channel_t *channel;
     fluid_gen_t gen[GEN_LAST];
     fluid_mod_t mod[FLUID_NUM_MOD];
-    int mod_count;
+    uint8_t mod_count;
     int has_looped; /* Flag that is set as soon as the first loop is completed.
                      */
     fluid_sample_t *sample;
-    int check_sample_sanity_flag; /* Flag that initiates, that sample-related
+    uint8_t check_sample_sanity_flag; /* Flag that initiates, that sample-related
                      parameters have to be checked. */
-#if 0
-	/* Instead of keeping a pointer to a fluid_sample_t structure,
-	 * I think it would be better to copy the sample data in the
-	 * voice structure. SoundFont loader then do not have to
-	 * allocate and maintain the fluid_sample_t structure. [PH]
-	 *
-	 * The notify callback may be used also for streaming samples.
-	 */
-	short* sample_data;             /* pointer to the sample data */
-	int sample_data_offset;         /* the offset of data[0] in the whole sample */
-	int sample_data_length;         /* the length of the data array */
-	unsigned int sample_start;
-	unsigned int sample_end;
-	unsigned int sample_loopstart;
-	unsigned int sample_loopend;
-	unsigned int sample_rate;
-	int sample_origpitch;
-	int sample_pitchadj;
-	int sample_type;
-	int (*sample_notify)(fluid_voice_t* voice, int reason);
-	void* sample_userdata;
-#endif
 
     /* basic parameters */
     fluid_real_t output_rate; /* the sample rate of the synthesizer */
@@ -97,11 +74,10 @@ struct _fluid_voice_t {
     /* End temporary variables */
 
     /* basic parameters */
-    fluid_real_t pitch;       /* the pitch in midicents */
-    fluid_real_t attenuation; /* the attenuation in centibels */
-    fluid_real_t
-        min_attenuation_cB; /* Estimate on the smallest possible attenuation
-                             * during the lifetime of the voice */
+    fluid_real_t pitch;              /* the pitch in midicents */
+    fluid_real_t attenuation;        /* the attenuation in centibels */
+    fluid_real_t min_attenuation_cB; /* Estimate on the smallest possible attenuation
+                                      * during the lifetime of the voice */
     fluid_real_t root_pitch;
 
     /* sample and loop start and end points (offset in sample memory).  */
@@ -117,7 +93,7 @@ struct _fluid_voice_t {
     /* vol env */
     fluid_env_data_t volenv_data[FLUID_VOICE_ENVLAST];
     unsigned int volenv_count;
-    int volenv_section;
+    uint8_t volenv_section;
     fluid_real_t volenv_val;
     fluid_real_t amplitude_that_reaches_noise_floor_nonloop;
     fluid_real_t amplitude_that_reaches_noise_floor_loop;
@@ -125,7 +101,7 @@ struct _fluid_voice_t {
     /* mod env */
     fluid_env_data_t modenv_data[FLUID_VOICE_ENVLAST];
     unsigned int modenv_count;
-    int modenv_section;
+    uint8_t modenv_section;
     fluid_real_t modenv_val; /* the value of the modulation envelope */
     fluid_real_t modenv_to_fc;
     fluid_real_t modenv_to_pitch;
@@ -147,16 +123,15 @@ struct _fluid_voice_t {
     fluid_real_t viblfo_to_pitch;
 
     /* resonant filter */
-    fluid_real_t
-        fres; /* the resonance frequency, in cents (not absolute cents) */
+    fluid_real_t fres;      /* the resonance frequency, in cents (not absolute cents) */
     fluid_real_t last_fres; /* Current resonance frequency of the IIR filter */
     /* Serves as a flag: A deviation between fres and last_fres */
     /* indicates, that the filter has to be recalculated. */
     fluid_real_t q_lin;        /* the q-factor on a linear scale */
     fluid_real_t filter_gain;  /* Gain correction factor, depends on q */
     fluid_real_t hist1, hist2; /* Sample history for the IIR filter */
-    int filter_startup;        /* Flag: If set, the filter will be set directly.
-                  Else it changes smoothly. */
+    uint8_t filter_startup;    /* Flag: If set, the filter will be set directly. Else it changes
+                                  smoothly. */
 
     /* filter coefficients */
     /* The coefficients are normalized to a0. */
@@ -190,19 +165,17 @@ int delete_fluid_voice(fluid_voice_t *voice);
 
 void fluid_voice_start(fluid_voice_t *voice);
 
-int fluid_voice_write(fluid_voice_t *voice, fluid_real_t *left,
-                      fluid_real_t *right, fluid_real_t *reverb_buf);
+int fluid_voice_write(fluid_voice_t *voice, fluid_real_t *left, fluid_real_t *right,
+                      fluid_real_t *reverb_buf);
 
-int fluid_voice_init(fluid_voice_t *voice, fluid_sample_t *sample,
-                     fluid_channel_t *channel, int key, int vel,
-                     unsigned int id, unsigned int time, fluid_real_t gain);
+int fluid_voice_init(fluid_voice_t *voice, fluid_sample_t *sample, fluid_channel_t *channel,
+                     int key, int vel, unsigned int id, unsigned int time, fluid_real_t gain);
 
 int fluid_voice_modulate(fluid_voice_t *voice, int cc, int ctrl);
 int fluid_voice_modulate_all(fluid_voice_t *voice);
 
 /** Set the NRPN value of a generator. */
-int fluid_voice_set_param(fluid_voice_t *voice, int gen, fluid_real_t value,
-                          int abs);
+int fluid_voice_set_param(fluid_voice_t *voice, int gen, fluid_real_t value, int abs);
 
 /** Set the gain. */
 int fluid_voice_set_gain(fluid_voice_t *voice, fluid_real_t gain);
@@ -217,42 +190,38 @@ int fluid_voice_noteoff(fluid_voice_t *voice);
 int fluid_voice_off(fluid_voice_t *voice);
 int fluid_voice_calculate_runtime_synthesis_parameters(fluid_voice_t *voice);
 fluid_channel_t *fluid_voice_get_channel(fluid_voice_t *voice);
-int calculate_hold_decay_buffers(fluid_voice_t *voice, int gen_base,
-                                 int gen_key2base, int is_decay);
+int calculate_hold_decay_buffers(fluid_voice_t *voice, int gen_base, int gen_key2base,
+                                 int is_decay);
 int fluid_voice_kill_excl(fluid_voice_t *voice);
+fluid_real_t fluid_voice_get_lower_boundary_for_attenuation(fluid_voice_t *voice);
 fluid_real_t
-fluid_voice_get_lower_boundary_for_attenuation(fluid_voice_t *voice);
-fluid_real_t
-fluid_voice_determine_amplitude_that_reaches_noise_floor_for_sample(
-    fluid_voice_t *voice);
+fluid_voice_determine_amplitude_that_reaches_noise_floor_for_sample(fluid_voice_t *voice);
 void fluid_voice_check_sample_sanity(fluid_voice_t *voice);
 
-#define fluid_voice_set_id(_voice, _id)                                        \
-    { (_voice)->id = (_id); }
+#define fluid_voice_set_id(_voice, _id)                                                            \
+    {                                                                                              \
+        (_voice)->id = (_id);                                                                      \
+    }
 #define fluid_voice_get_chan(_voice) (_voice)->chan
 
-#define _PLAYING(voice)                                                        \
-    (((voice)->status == FLUID_VOICE_ON) ||                                    \
-     ((voice)->status == FLUID_VOICE_SUSTAINED))
+#define _PLAYING(voice)                                                                            \
+    (((voice)->status == FLUID_VOICE_ON) || ((voice)->status == FLUID_VOICE_SUSTAINED))
 
 /* A voice is 'ON', if it has not yet received a noteoff
  * event. Sending a noteoff event will advance the envelopes to
  * section 5 (release). */
-#define _ON(voice)                                                             \
-    ((voice)->status == FLUID_VOICE_ON &&                                      \
-     (voice)->volenv_section < FLUID_VOICE_ENVRELEASE)
+#define _ON(voice)                                                                                 \
+    ((voice)->status == FLUID_VOICE_ON && (voice)->volenv_section < FLUID_VOICE_ENVRELEASE)
 #define _SUSTAINED(voice) ((voice)->status == FLUID_VOICE_SUSTAINED)
-#define _AVAILABLE(voice)                                                      \
-    (((voice)->status == FLUID_VOICE_CLEAN) ||                                 \
-     ((voice)->status == FLUID_VOICE_OFF))
+#define _AVAILABLE(voice)                                                                          \
+    (((voice)->status == FLUID_VOICE_CLEAN) || ((voice)->status == FLUID_VOICE_OFF))
 #define _RELEASED(voice) ((voice)->chan == NO_CHANNEL)
 #define _SAMPLEMODE(voice) ((int)(voice)->gen[GEN_SAMPLEMODE].val)
 
 fluid_real_t fluid_voice_gen_value(fluid_voice_t *voice, int num);
 
-#define _GEN(_voice, _n)                                                       \
-    ((fluid_real_t)(_voice)->gen[_n].val +                                     \
-     (fluid_real_t)(_voice)->gen[_n].mod +                                     \
+#define _GEN(_voice, _n)                                                                           \
+    ((fluid_real_t)(_voice)->gen[_n].val + (fluid_real_t)(_voice)->gen[_n].mod +                   \
      (fluid_real_t)(_voice)->gen[_n].nrpn)
 
 #define FLUID_SAMPLESANITY_CHECK (1 << 0)
