706158884aab6c251a1b393ee9e80b6d2ace9311
yuan <yuan_xin_yu@hotmail.com>
2025-02-25 09:50:55 +0800

    Update c-cpp.yml add apt-get update

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -6,5 +6,6 @@ set -x  # 启用命令打印
 sudo apt-get update
 sudo apt install build-essential libc6-dev
 sudo dpkg --add-architecture i386
+sudo apt-get update
 sudo apt install gcc-multilib g++-multilib libc6-dbg:i386
 
