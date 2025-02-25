59b12b9db64134a8f29c3a95e0b3f0b6a0d5a822
yuan <yuan_xin_yu@hotmail.com>
2025-02-25 10:59:47 +0800

    Windows CI remove clang

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -4,7 +4,7 @@ set -x  # 启用命令打印
 
 
 sudo apt-get update
-sudo apt install build-essential libc6-dev
+sudo apt install build-essential libc6-dev ffmpeg
 sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt install gcc-multilib g++-multilib libc6-dbg:i386
