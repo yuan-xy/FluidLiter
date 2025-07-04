c3c28e418414536f6224f302c38d6ad9f6674018
yuan <yuan_xin_yu@hotmail.com>
2025-07-04 09:20:06 +0800

    remove workaround for snprint on MSVC <2015

--- src/fluid_synth.c
+++ src/fluid_synth.c
@@ -2404,33 +2404,11 @@ int fluid_synth_tuning_iteration_next(fluid_synth_t *synth, int *bank,
     return 0;
 }
 
-// workaround for snprint on MSVC <2015 from
-// http://stackoverflow.com/questions/2915672/snprintf-and-visual-studio-2010
-#if defined(_MSC_VER) && _MSC_VER < 1900
-#define snprintf c99_snprintf
-#define vsnprintf c99_vsnprintf
 
-__inline int c99_vsnprintf(char *outBuf, size_t size, const char *format,
-                           va_list ap) {
-    int count = -1;
-
-    if (size != 0) count = _vsnprintf_s(outBuf, size, _TRUNCATE, format, ap);
-    if (count == -1) count = _vscprintf(format, ap);
-
-    return count;
-}
-
-__inline int c99_snprintf(char *outBuf, size_t size, const char *format, ...) {
-    int count;
-    va_list ap;
-
-    va_start(ap, format);
-    count = c99_vsnprintf(outBuf, size, format, ap);
-    va_end(ap);
-
-    return count;
-}
+#if defined(_MSC_VER)
+_Static_assert(_MSC_VER > 1900, "Nolonger support MSVC <2015");
 #endif
+
 
 int fluid_synth_tuning_dump(fluid_synth_t *synth, int bank, int prog,
                             char *name, int len, double *pitch) {
